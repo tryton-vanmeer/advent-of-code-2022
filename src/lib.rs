57542a9c92962ff794f86352847b832a69b8caa5
Tryton Van Meer <code@trytonvanmeer.dev>
2022-12-16 23:49:35 -0500

    add comment with basic loop for input files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use std::path::Path;
 
+// if let Ok(lines) = std::fs::read_to_string(get_input_file()) {
+//     for line in lines.split('\n') {
+//         // do stuff here
+//     }
+// }
 pub fn get_input_file() -> String {
     let args: Vec<String> = std::env::args().collect();
     let basename = Path::new(&args[0]).file_stem().unwrap().to_str().unwrap();
