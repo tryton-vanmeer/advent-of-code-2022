c11b8b9940abcb15e78089eed3dfa8433abdddd1
Tryton Van Meer <code@trytonvanmeer.dev>
2022-12-16 23:10:52 -0500

    get text file based on bin name instead of clap args. accept a function as arg and DRY the input file iterator.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -5,4 +7,21 @@ use clap::Parser;
 pub struct Args {
     #[arg()]
     pub input: String,
+}
+
+pub fn get_bin_name() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    let basename = Path::new(&args[0]).file_stem().unwrap();
+
+    basename.to_str().unwrap().to_string()
+}
+
+pub fn input_iterator<F: Fn(&str)>(execute: F) {
+    let input_filename = format!("inputs/{}.txt", get_bin_name());
+
+    if let Ok(lines) = std::fs::read_to_string(input_filename) {
+        for line in lines.split('\n') {
+            execute(line);
+        }
+    }
 }
