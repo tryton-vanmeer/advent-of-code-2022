42f43a0d329aa1360fbbdb84a473bb93b62af57d
Tryton Van Meer <code@trytonvanmeer.dev>
2023-03-02 20:39:14 -0500

    create struct for rucksacks and func for creating them from input line.

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -1,9 +1,29 @@
+#[derive(Clone, Debug)]
+struct Rucksack {
+    fist_compartment: String,
+    second_compartment: String,
+}
+
+impl Rucksack {
+    fn from(contents: &str) -> Rucksack {
+        let compartment_size = contents.len() / 2;
+
+        Rucksack {
+            fist_compartment: contents[0..compartment_size].to_string(),
+            second_compartment: contents[compartment_size..contents.len()].to_string(),
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let input: &'static str = include_str!("../../inputs/day_3.txt");
+    let mut rucksacks: Vec<Rucksack> = vec![];
 
     for line in input.lines() {
-        println!("{}", line);
+        rucksacks.push(Rucksack::from(line));
     }
+
+    println!("{:#?}", rucksacks);
 
     Ok(())
 }
