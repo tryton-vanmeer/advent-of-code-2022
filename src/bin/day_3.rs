c4f0222c357e09bc50fefd455483090a13fd8a27
Tryton Van Meer <code@trytonvanmeer.dev>
2023-03-02 20:48:42 -0500

    find shared item when from'ing Rucksack.

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -2,15 +2,30 @@
 struct Rucksack {
     fist_compartment: String,
     second_compartment: String,
+    shared_item: char,
+}
+
+fn find_shared_item(first: &str, second: &str) -> Option<char> {
+    for item in first.chars() {
+        if second.contains(item) {
+            return Some(item)
+        }
+    }
+
+    None
 }
 
 impl Rucksack {
     fn from(contents: &str) -> Rucksack {
         let compartment_size = contents.len() / 2;
 
+        let first = &contents[0..compartment_size];
+        let second = &contents[compartment_size..contents.len()];
+
         Rucksack {
-            fist_compartment: contents[0..compartment_size].to_string(),
-            second_compartment: contents[compartment_size..contents.len()].to_string(),
+            fist_compartment: first.to_string(),
+            second_compartment: second.to_string(),
+            shared_item: find_shared_item(first, second).unwrap()
         }
     }
 }
