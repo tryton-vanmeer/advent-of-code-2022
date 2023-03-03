948f2ccd12a7aee09d67c42cdda99d9c2936a3f0
Tryton Van Meer <code@trytonvanmeer.dev>
2023-03-02 21:29:29 -0500

    replace find_shared_items func with calling find on first chars.

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -1,18 +1,6 @@
 #[derive(Clone, Debug)]
 struct Rucksack {
-    // fist_compartment: String,
-    // second_compartment: String,
     shared_item: char,
-}
-
-fn find_shared_item(first: &str, second: &str) -> Option<char> {
-    for item in first.chars() {
-        if second.contains(item) {
-            return Some(item)
-        }
-    }
-
-    None
 }
 
 impl Rucksack {
@@ -23,9 +11,7 @@ impl Rucksack {
         let second = &contents[compartment_size..contents.len()];
 
         Rucksack {
-            // fist_compartment: first.to_string(),
-            // second_compartment: second.to_string(),
-            shared_item: find_shared_item(first, second).unwrap()
+            shared_item: first.chars().find(|&item| second.contains(item)).unwrap(),
         }
     }
 }
